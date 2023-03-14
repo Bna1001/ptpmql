deef2f1020d21a960806a5baa4fb5d0ddbcf63ea
unknown <namanhbui1001@gmail.com>
2023-03-15 01:45:46 +0700

    commit note

---
+++ baithuchanh1303/baithuchanh1303/Models/Process/GiaiPhuongTrinh.cs
@@ -0,0 +1,11 @@
+﻿namespace baithuchanh1303.Models.Process
+{
+    public class GiaiPhuongTrinh
+    {
+        public string GiaiPhuongTrinhB1(double a,double b)
+        {
+            double x = -b / a;
+            return "Nghiem cua phuong trinh x la: " + x;
+        }
+    }
+}
